bf6bf1b955200b04dd692ac0669339246f4d9c4c
Jorge Domínguez de Anda <jorge.dm97@gmail.com>
2019-09-06 12:31:18 -0500

    added-abstract-class

--- Redmine.library/Models/ProjectListResponse.cs
+++ Redmine.library/Models/ProjectListResponse.cs
@@ -4,16 +4,9 @@ using System.Text;
 
 namespace Redmine.library.Models
 {
-    public class ProjectListResponse
+    public class ProjectListResponse : ResultContent
 
     {
-        public List<Project> Projects { get; set; }
-        // todo: it appears this properties will appear in different resources (projects, issues, time entries, etc)
-        // so it would make sense to move them to an abstract class named something like ResultContent and inherit it.
-        // todo: keep consistency in naming. Rename to TotalCount instead. 
-        public int TotalCount { get; set; }
-        public int Offset { get; set; }
-        public int Limit { get; set; }
 
     }
 }
---
+++ Redmine.library/Models/ResultContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Redmine.library.Models
+{
+    public abstract class ResultContent 
+    {
+
+        public List<Project> Projects { get; set; }
+        public int TotalCount { get; set; }
+        public int Offset { get; set; }
+        public int Limit { get; set; }
+    }
+}
