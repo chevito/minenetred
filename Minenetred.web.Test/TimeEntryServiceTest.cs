c6ba6628a7dfd8ce9e9577358177f32756c280db
Jorge Domínguez de Anda <jorge.dm97@gmail.com>
2019-10-04 11:39:23 -0500

    aditional-tests

---
+++ Minenetred.web.Test/TimeEntryServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Minenetred.web.Test
+{
+    public class TimeEntryServiceTest
+    {
+        [Fact]
+        public void ShouldReturnTotalHoursPerDateAndProject()
+        {
+
+        }
+    }
+}
