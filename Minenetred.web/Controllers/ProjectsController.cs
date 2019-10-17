ce51c85ef7210827bac812a18cf5e7466e8f87fe
Jorge Domínguez de Anda <jorge.dm97@gmail.com>
2019-10-17 09:54:05 -0500

    requested-changes

--- Minenetred.web/Controllers/ProjectsController.cs
+++ Minenetred.web/Controllers/ProjectsController.cs
@@ -57,7 +57,6 @@ namespace Minenetred.web.Controllers
             }
             catch (Exception)
             {
-                
                 return RedirectToAction("AddKey", new {msj = "Add a valid key"});
             }
         }
