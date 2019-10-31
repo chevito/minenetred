a56d77d5fb9e47eff3e8a9c3f11ed6afe1914fbd
Jorge Domínguez de Anda <jorge.dm97@gmail.com>
2019-10-31 15:58:39 -0600

    merging-with-dev

--- Minenetred.web/Controllers/ProjectsController.cs
+++ Minenetred.web/Controllers/ProjectsController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.DirectoryServices.AccountManagement;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Minenetred.Web.Controllers
 {
